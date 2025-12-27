03a4c6f455fb21aa8bc6890d2f2a2321c427c450
lucasleon <lucasleon2107@gmail.com>
2025-12-27 19:07:07 +0100

    Add sort-colors solution

--- 075-sort-colors/solution.kt
+++ 075-sort-colors/solution.kt
@@ -1,31 +1,26 @@
 // LeetCode 75: sort-colors
 
 fun sortColors(nums: IntArray): Unit {
-    var zero = 0
-    var one = 0
-
-    nums.forEach { num ->
-        when (num) {
-            0 -> zero++
-            1 -> one++
-        }
+    fun swap(left: Int, right: Int) {
+        val temp = nums[left]
+        nums[left] = nums[right]
+        nums[right] = temp
     }
 
-    nums.indices.forEach { index ->
-        when {
-            zero > 0 -> {
-                nums[index] = 0
-                zero--
-            }
+    var left = 0
+    var right = nums.lastIndex
+    var i = 0
 
-            one > 0 -> {
-                nums[index] = 1
-                one--
-            }
-
-            else -> {
-                nums[index] = 2
-            }
+    while (i <= right) {
+        if (nums[i] == 0) {
+            swap(left, i)
+            left++
+        } else if (nums[i] == 2) {
+            swap(i, right)
+            right--
+            i--
         }
+
+        i++
     }
 }
